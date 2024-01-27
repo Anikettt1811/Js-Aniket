24494a2ce134a78aad4ddebc30c248bae97dbf84
Aniket Raj <103485159+Anikettt1811@users.noreply.github.com>
2024-01-27 17:13:58 +0000

    abt_datatypes

---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Hitesh")
+
+
+let name = "hitesh"         // String
+let age = 18                // Number
+let isLoggedIn = false      // Boolean
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => "" ** String ko humesha quotes ke andar likhna hota h **
+// boolean => true/false
+// null => standalone value 
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
