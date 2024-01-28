e9e8d3f6bd581a606d3e85fd87a7c848e0baf609
Aniket Raj <103485159+Anikettt1811@users.noreply.github.com>
2024-01-28 12:31:13 +0000

    operations

--- 01_basic/03_conversion_operations.js
+++ 01_basic/03_conversion_operations.js
@@ -6,7 +6,7 @@ let score = "aniket"
 
 let valueInNumber = Number(score)
 //console.log(typeof valueInNumber); output- Number
-console.log(valueInNumber); //output-Nan (Not a Number)
+// console.log(valueInNumber); //output-Nan (Not a Number)
 
 
 // "33" => 33
@@ -25,5 +25,45 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = null
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);    // addition
+// console.log(2-2);    //substraction
+// console.log(2*2);    //multiplication
+// console.log(2**3);   //power
+// console.log(2/3);    //divide
+// console.log(2%3);    //modulus
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
